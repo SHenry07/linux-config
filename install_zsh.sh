7860fd0fa3109400558417524c55a0a934697e32
Henry Sun <sunheng1235@gmai.com>
2020-11-21 22:27:36 +0800

    feat: version 1 for zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+
+DIR=$(cd $(dirname $0) && pwd)
 OH_MY_ZSH_FOLDER="${HOME}/.oh-my-zsh"
 
-if [ ! -d "${OH_MY_ZSH_FOLDER}" ] ; then
+if [ `cat /etc/shells | grep -w zsh` == "" ] ; then
+  sudo apt install zsh
+fi
+
+if [[ ! -d "${OH_MY_ZSH_FOLDER}" ]] ; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
     echo "oh-my-zsh already installed"; # Should I prompt for repull?
 fi
 
 
-/bin/zsh source ~/.zshrc
 OH_MY_ZSH_PLUGINS_FOLDER=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins
 
 # Oh My Zsh
@@ -18,7 +23,6 @@ OH_MY_ZSH_PLUGINS_FOLDER=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins
 # 中国大陆用户可以使用 gitee.com 上的官方镜像加速下载.
 
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i  's#^ZSH_THEME=.*$#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc
 ## zsh-autosugestions
 ZSH_SUGGESTIONS_GIT_URL="https://github.com.cnpmjs.org/zsh-users/zsh-autosuggestions"
 git clone --depth=1 $ZSH_SUGGESTIONS_GIT_URL "${OH_MY_ZSH_PLUGINS_FOLDER}/zsh-autosuggestions"
@@ -26,3 +30,6 @@ git clone --depth=1 $ZSH_SUGGESTIONS_GIT_URL "${OH_MY_ZSH_PLUGINS_FOLDER}/zsh-au
 ## zsh-syntax-highlighting
 ZSH_SYNTAX_HIGHLIGHTING_GIT_URL="https://github.com.cnpmjs.org/zsh-users/zsh-syntax-highlighting.git"
 git clone --depth=1 $ZSH_SYNTAX_HIGHLIGHTING_GIT_URL "${OH_MY_ZSH_PLUGINS_FOLDER}/zsh-syntax-highlighting"
+
+sudo apt install autojump
+cp ${DIR}/.* ~/
